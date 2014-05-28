283dabc810bfb5dda7bfac1f2b6445ae2c8ad34a
Fattahul Alam <faruk@nascenia.com>
2014-05-28 19:37:11 +0600

    used separate transaction prefix for test and production environment

--- api/dbbl_lib.php
+++ api/dbbl_lib.php
@@ -34,7 +34,12 @@ class DbblLib
 
     function create_transaction($amount, $description, $mrch_transaction_id, $provider, $retry_count = 5)
     {
-        $transaction_command = $this->transaction_command($amount, $description, $this->merchant_transaction_id_prefix . $mrch_transaction_id, $provider);
+        if ($this->environment == "production") {
+            $prefix = $this->merchant_transaction_id_prefix;
+        } else {
+            $prefix = $this->merchant_transaction_id_prefix . "test-";
+        }
+        $transaction_command = $this->transaction_command($amount, $description, $prefix . $mrch_transaction_id, $provider);
 //logger . info "Transaction command: #{transaction_command}"
 //$transaction_command = "curl --get --data command=#{CGI::escape(transaction_command)} #{DBBL_CONFIG["command_runner_url"]}";
 //logger . info "Transaction command: #{transaction_command}"
